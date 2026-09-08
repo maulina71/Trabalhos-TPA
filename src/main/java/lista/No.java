2497ab117683cffa821d854bc714f0f884156b67
maulina71 <gabidemezio2@gmail.com>
2026-09-08 16:32:15 -0300

    Update No.java

--- src/main/java/lista/No.java
+++ src/main/java/lista/No.java
@@ -1,1 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+//  */
 
+// CODIGO FORNECIDO NA ESPECIFICAÇÃO
+
+package listaencadeada;
+
+/**
+ *
+ * @author victoriocarvalho
+ */
+public class No<T> {
+    private T valor;
+    private No<T> prox;
+
+    
+    public No(T valor){
+        this.valor = valor;
+        this.prox=null;
+    }
+    /**
+     * @return the valor
+     */
+    public T getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(T valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the prox
+     */
+    public No<T> getProx() {
+        return prox;
+    }
+
+    /**
+     * @param prox the prox to set
+     */
+    public void setProx(No<T> prox) {
+        this.prox = prox;
+    }
+    
+    
+}
